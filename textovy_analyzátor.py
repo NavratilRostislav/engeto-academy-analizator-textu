87560e5e13449e51d1eee55ff3a853edda9896cc
Rostislav Navrátil <35820137+NavratilRostislav@users.noreply.github.com>
2024-11-06 18:55:54 +0100

    Add files via upload

--- textovy_analyzátor.py
+++ textovy_analyzátor.py
@@ -1,4 +1,4 @@
-""""
+"""
 projekt.py : první projekt do Engeto Online Python Akadenie
 
 autor: Rostislav Navrátil
@@ -6,11 +6,14 @@ email: navratil.rostislav@email.cz
 discord: Rostislav Navrátil
 """
 
-  #importuj TEXTS z task_templace
+
+#importuj TEXTS z task_templace
 
 from task_template import TEXTS 
 import re
-# databaze uživatelu a hesel
+
+
+# databaze uživatelů a hesel
 
 uzivatele = {
     "bob": "123", 
@@ -19,155 +22,140 @@ uzivatele = {
     "liz": "pass123"        
 }
 
-
+#základní seznámení uživatele
+print("vítej uživately v analyzátoru textu")
+print("-"*20)
 
 #vstupy od uživatele
 uzivatelske_jmeno = input("zadej své uživatelské jmeno: ")
 heslo = input("zadej své uživatelské heslo: ")
 
-print("-"*20)
+print("-" * 20)
 
-# Ověření uživatele
-def overeni_uzivatele(uzivatelske_jmeno, heslo):
-    print(f"Kontrola uživatelského jména: {uzivatelske_jmeno}")
-    print(f"Kontrola hesla: {heslo}")
-
-    print("-"*20)
 
 #ověření uživatele
-#def overeni_uzivatele(uzivatelske_jmeno, heslo):
-    if uzivatelske_jmeno in uzivatele:
-        print(f"uzivatel {uzivatelske_jmeno} nalezen") 
-
-        if uzivatele[uzivatelske_jmeno] == heslo:
-            #print("heslo je spravne")
-            return True
-        else:
-            print("spatne helso")
-            return False
+def overeni_uzivatele(uzivatelske_jmeno, heslo):
+    if uzivatelske_jmeno in uzivatele and uzivatele[uzivatelske_jmeno] == heslo:
+        print(f"uživatel {uzivatelske_jmeno} nalezen a heslo je správné.") 
+        print("Úspěšné přihlášení!")
+        print(f"vítám tě zpátky {uzivatelske_jmeno}. sem rád že ses vrátil/a.")
+        print("-" * 20)    
+        return True
     else:
         print("špatné jméno nebo heslo. Program se ukončí.")
         exit()      
-        
-# Volání funkce pro ověření uživatele
-if overeni_uzivatele(uzivatelske_jmeno, heslo):
-    print("Úspěšné přihlášení!")
-    print("-"*20)
-
+   
 #výběr textu
-    def vyber_text(texty):
+def vyber_text(texty):
 
     #vu = výběr uživatele
+    vu = input("vyber text který chceš analyzovat čísla 1-3 :")
+    print("-" * 20)
+    if vu in ["1", "2", "3"]:
+        vybrany_text = texty[int(vu)-1]  #získání textu podle výběru 
+        
+        #ověření a převod na řetězec
+        if isinstance(vybrany_text, list):
+            vybrany_text = " ".join(vybrany_text)
+        elif not isinstance(vybrany_text, str):
+            raise ValueError("chyba v 'vyber_text' : Očekáván byl řetězec.")
+        
+        print(f"vybraný text:\n{vybrany_text}\n" + "-" * 20)
+        return vybrany_text
+    else:
+        print("neplatný výběr. Zavírám program")
+        exit()
 
-        vu = input("vyber text který chceš analyzovat čísla 1-3 :")
-        print("-"*20)
 
-        if vu in ["1","2","3"]:
-            analyzovany_text = texty[int(vu)-1]  #získání textu podle výběru 
-            return analyzovany_text
+#odstraní všechny nežádoucí znaky kromě písmen a mezer
+def odstran_nezadouci_znaky(vybrany_text):
+    return re.sub(r"[^a-zA-Z0-9\s]", "", vybrany_text)
+
+#počítají se jen slova z alfabeth 
+def is_strictly_regex(words):
+    return all(bool(re.fullmatch(r"[A-Z]+", word)) for word in words)
+
+
+#Analýza vybraného textu
+def analyza_textu(vybrany_text):
+    if isinstance(vybrany_text, list):#zkontroluje jestly je vybrany_text seznam
+        vybrany_text = " ".join(vybrany_text) #pokud ano, převedeme ho na řetezec  
+
+    #vyčistění textu
+    cisty_text = odstran_nezadouci_znaky(vybrany_text)
+    #print(f"zkušební čisty text: {cisty_text}")#pomocný print
     
-        else:
-            print("neplatný výběr. Zavírám program")
-            exit()
-
-analyzovany_text = vyber_text(TEXTS)
-print(f"vybraný text:\n{analyzovany_text}")
-
-#def analyzy_textu
-def analýza_textu(text):
     #proměnné statistik
-
-    total_words = 0
+    words = cisty_text.split()#rozdělí text na slova a uloží do seznamu
+    total_words = len(words)#spočítá kolik slov je v seznamu
     capitalized_words = 0
     uppercase_words = 0
     lowercase_words = 0
     total_numbers = 0
     sum_numbers = 0   
     
-    #rozdělí text na slova a uloží do seznamu
-    words = analyzovany_text.split()
-    #spočítá kolik slov je v seznamu 
-    total_words = len(words)
+    #print(f"výpis všech slov: {words}")#pomocný print
+
     #smička projde každe slovo v seznamu words
     for word in words:
         if word[0].isupper():
             capitalized_words += 1 #počet slov začínajcím velkým písmenem
-        
-        if word.isupper():
+        if is_strictly_regex(word):
             uppercase_words += 1 #počet slov psaných velkým písmenem 
-        
         if word.islower():
             lowercase_words +=1 # počet slov psaných malím písmenem 
-
         if re.match(r'^\D*\d+\D*$',word): # slovo obsahující čísla 
         #počet čísel se zvyšuje a sčítají se všechny číslice nalezené ve slově
             total_numbers+= 1
         sum_numbers += sum(int(num)for num in re.findall(r'\d+',word))
     
-    print("-"*20)
+    print("-" * 20)
     print(f"1.Celkový počet slov:{total_words} ")
     print(f"2.počet slov začínajících velkým písmenem:{capitalized_words}")
     print(f"3.počet slov psaných velkými písmeny:{uppercase_words} ")
     print(f"4.počet slov psaných malými písmeny:{lowercase_words} ")
     print(f"5.počet čísel (ne cifer):{ total_numbers}")
     print(f"6.suma všech čísel (ne cifer) v textu:{sum_numbers}")
-    print("-"*20)
+    print("-" * 20)
     
     
+    #vypsání zakladního sloupcoveho grafu četnosti delky slov
+    print("základní sloupcový graf délky slov")
+    print("-" * 20)
+    
+    # proměné pro počítání slov různých délek
+    pismena = [0] * 10 # pole pro délky slov 1-10
+    
+    # počítání delky slov
+    for word in words:
+        delka = len(word)
+        if 1 <= delka < len(pismena):
+            pismena[delka]+= 1
+        
+    #výpis výsledků
+    print("LEN|     OCCURENCES       |  NR.")
+    for i in range(1, len(pismena)):
+        print(f"{i}  |{"*" * pismena[i]:<20}  |  {pismena[i]}")
+    #print("-"*20)
+    
+    #funkce do budoucna   
+    #povolene_delky = [5] #povolene delky na výpis 
+    #for word in words:
+        #if len(word) in povolene_delky:
+           #print(word)
 
-#zavolejte funkci pro analýzu textu
-analýza_textu(vyber_text)  
-
-#funkce na vypsání zakladního sloupcoveho grafu četnosti delky slov
-print("základní sloupcový graf délky slov")
-print("-"*20)
-
-# proměná pro slova ve slovníku 
-words = []
-#proměné pro počítání slov různých délek
-
-pismena_1 = 0
-pismena_2 = 0
-pismena_3 = 0
-pismena_4 = 0
-pismena_5 = 0
-pismena_6 = 0
-pismena_7 = 0
-pismena_8 = 0
-pismena_9 = 0
-
-words = analyzovany_text.split()
-
-for word in words:
-    delka = len(word)
-    if delka == 1:
-        pismena_1 += 1
-    elif delka == 2:
-        pismena_2 += 1
-    elif delka == 3:
-        pismena_3 += 1
-    elif delka == 4:
-        pismena_4 += 1
-    elif delka == 5:
-        pismena_5 += 1
-    elif delka == 6:
-        pismena_6 += 1
-    elif delka == 7:
-        pismena_7 += 1
-    elif delka == 8:
-        pismena_8 += 1
-    elif delka == 9:
-        pismena_9 +=1
-
-#výpis výsledků
-print("LEN|     OCCURENCES      | NR.")
-print(f"1  |{"*" * pismena_1:<20} | {pismena_1}")
-print(f"2  |{"*" * pismena_2:<20} | {pismena_2}")
-print(f"3  |{"*" * pismena_3:<20} | {pismena_3}")
-print(f"4  |{"*" * pismena_4:<20} | {pismena_4}")
-print(f"5  |{"*" * pismena_5:<20} | {pismena_5}")
-print(f"6  |{"*" * pismena_6:<20} | {pismena_6}")
-print(f"7  |{"*" * pismena_7:<20} | {pismena_7}")
-print(f"8  |{"*" * pismena_8:<20} | {pismena_8}")
-print(f"9  |{"*" * pismena_9:<20} | {pismena_9}")
-
+    #zavolání funkce pro odsranění nežádoucích znaků
+    odstran_nezadouci_znaky(vybrany_text)
+    
+    #zavolání funkce: počítají se jen slova z alfabeth
+    is_strictly_regex(words)
+           
+if overeni_uzivatele(uzivatelske_jmeno, heslo):
+    vybrany_text = vyber_text(TEXTS)
+    if not isinstance(vybrany_text, str):
+        print("vybraný text není správný, očekáván byl řetězec")
+        exit()
+    
+    
+    analyza_textu(vybrany_text)
+    
